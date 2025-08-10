4c638708b712deb6a2ab974d474bcfefe65f8f97
Andre Luiz da Rosa <andreluiznefi@gmail.com>
2025-08-10 01:42:16 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <span class="">
    
    w

--- src/components/landing/solution.tsx
+++ src/components/landing/solution.tsx
@@ -28,7 +28,6 @@ const ecosystemPackage = {
   description: "A solução definitiva para quem busca escalar, automatizar o atendimento e transformar clientes em uma máquina de indicações.",
   features: [
     "Tudo do pacote Starter +",
-    "Trafego pago 3 campanhas por mes",
     "Link da BIO (microsite)",
     "Página de captura",
     "CRM para gerenciamento",
